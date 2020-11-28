77f42474bfcb708bdfd37aca11b2671fdb2e4b4a
RichardThunder <yuetingpei@163.com>
2020-11-28 22:14:58 +0800

    Add project files.

---
+++ Picture.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Picture.aspx.cs" Inherits="Lab3.Picture" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Panel ID="Panel1" runat="server" Height="779px">
+            <asp:Button ID="First" runat="server" Text="首页" ForeColor="Red" OnClick="Button1_Click" />
+            &nbsp;&nbsp;&nbsp;<asp:Button ID="Former" runat="server" Text="上一张" OnClick="Former_Click" />
+            &nbsp; &nbsp;<asp:Button ID="Next" runat="server" OnClick="Next_Click" style="margin-left: 0px" Text="下一张" />
+            &nbsp;&nbsp;&nbsp;<asp:Button ID="Last" runat="server" Text="尾页" ForeColor="Red" OnClick="Last_Click" style="width: 40px" />
+            &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;<br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="图片"></asp:Label>
+            <br />
+            <asp:Image ID="Image1" runat="server" Height="600px" Width="800px" />
+            <br />
+            <br />
+            <br />
+            <asp:FileUpload ID="Upload" runat="server" Height="18px" Width="320px" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         
+            <asp:Button ID="Submit" runat="server" OnClick="Submit_Click" Text="Submit" />
+         
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Picture.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+namespace Lab3
+{
+    public partial class Picture : System.Web.UI.Page
+    {
+
+        static string fileName;
+        static int count= System.IO.Directory.GetFiles(@"G:/web/Lab3/Image/").Length;//读取文件夹下图片数量来初始化Count计数器
+        static int COUNTNOW = count;//当前页面位置
+        //int MAX = 100;//
+        //static String[] FILENAME = new String[100];//eexb
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            if(this.Upload.HasFile)
+            { 
+                count++;
+                COUNTNOW = count;
+                fileName = Upload.FileName;
+                string strExt = System.IO.Path.GetExtension(fileName);
+                string savePath = @"G:/web/Lab3/Image/" +count+".jpg";
+              
+                Upload.SaveAs(savePath);
+                Image1.ImageUrl = "http://127.0.0.1:1677/Image/" + count +".jpg";
+               
+            }
+        }
+        protected void Former_Click(object sender, EventArgs e)
+        {
+            if (COUNTNOW == 1)
+            {
+                System.Windows.Forms.MessageBox.Show("已经是第一个文件了!", "error");
+                return;
+            }
+            if (count ==0 )
+            {
+                System.Windows.Forms.MessageBox.Show("没有图片,请上传!", "error");
+                return;
+            }
+            COUNTNOW--;
+            Image1.ImageUrl = "http://127.0.0.1:1677/Image/" + COUNTNOW + ".jpg";
+           
+        }
+
+        protected void Next_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("没有图片,请上传!", "error");
+                return;
+            }
+            if (COUNTNOW == count)
+            {
+                System.Windows.Forms.MessageBox.Show("已经是最后一个文件了!", "error");
+                return;
+            }
+            COUNTNOW++;
+            Image1.ImageUrl = "http://127.0.0.1:1677/Image/" + COUNTNOW + ".jpg";
+        }
+
+        protected void Last_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("没有图片,请上传!", "error");
+                return;
+            }
+            Image1.ImageUrl = "http://127.0.0.1:1677/Image/" + count + ".jpg";
+            COUNTNOW = count;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (count == 0)
+            {
+                System.Windows.Forms.MessageBox.Show("没有图片,请上传!", "error");
+                return;
+            }
+            Image1.ImageUrl = "http://127.0.0.1:1677/Image/" + 1 +".jpg";
+            COUNTNOW = 1;
+        }
+    }
+}
